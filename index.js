1578e8d44d81d3ae29132a602d8079a12492ab85
Akshat Sharma <96696393+AkshatSharma078@users.noreply.github.com>
2022-08-29 15:49:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const fetchData = async()=>{
     const data = await res.json()
     document.getElementById('id').innerHTML = data.slip.id
     document.getElementById('advice').innerHTML = data.slip.advice
-    console.log(data)
     colors =['#472D2D','#2C3333','#2C3639','#4C3A51','#1B2430','#774360','#4C3575','#371B58','#2E0249','#16003B']
     document.body.style.background = `${colors[Math.floor(Math.random() * 10)]}`
 
